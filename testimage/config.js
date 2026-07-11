fc1ad5fa7aa351fd559205792188940859c704af
abibazhi <abibazhi@126.com>
2026-07-12 07:33:46 +0800

    数据整合

--- testimage/config.js
+++
@@ -1,5 +0,0 @@
-// 全局配置
-export const DISABLE_CACHE = true;
-export const IMG_TIMEOUT = 10000;
-export const R2_BASE = "https://pub-a0fc4de2782d448f916244913768fb78.r2.dev";
-export const GITHUB_BASE = "https://abibazhi.github.io";
